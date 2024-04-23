9de5de2e84057aeca4a2fabe73446a99c043bebb
alireza_cheraghali <cheraghalialireza33@gmail.com>
2024-04-23 17:45:06 +0330

    template

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+  ],
+  env: {
+    es2021: true,
+    node: true,
+  },
+  rules: {},
+  plugins: ['react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
 };
